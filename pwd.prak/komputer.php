affd2d42e69b9570dbdd7c282faa51cf8d428479
bagas551 <124250036@student.upnyk.ac.id>
2026-05-12 12:17:06 +0700

    fix

--- pwd.prak/komputer.php
+++ pwd.prak/komputer.php
@@ -5,19 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
     
 
 <style>
     body{
         margin: 0;
-        font-family: Poppins;
+        font-family: Arial, sans-serif;
     }
 
     /*background*/
     .bg {
         height: 100vh;
-        background: url('kelas.jpeg') center/cover no-repeat;
+        background: url('') center/cover no-repeat;
         display: flex;
         justify-content: center;
         align-items: center;
